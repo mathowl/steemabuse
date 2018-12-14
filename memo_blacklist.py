410b7dedebd11fa6447d947ed7bbe4a88652630f
mathowl <45641277+mathowl@users.noreply.github.com>
2018-12-14 18:51:44 +0100

    steem memos containing blacklist
    given a steem-user and #days, returns memos in which the word blacklist appears.

---
+++ memo_blacklist.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from beem.account import Account
+from beem.amount import Amount
+
+
+
+def memos_blacklist(user_c,m_days):
+    
+    """
+    Given a steem-user and #days, returns memos in which the word blacklist appears.
+    :param str user_c: steem user
+    :param int m_days: number of days 
+    :rtype: list str with memos containing blacklist
+    """
+
+    acc = Account(user_c)
+    stop_time = datetime.now()
+    start_time = stop_time - timedelta(m_days)
+    blist = []
+    mem_memo = set()
+    
+    for mem in acc.history(start=start_time, stop=stop_time,only_ops=['transfer']):
+        
+        #make the text lower case
+        newmem = mem['memo'].lower()
+        # find memos which contain the word blacklist and only consider unique transaction senders
+        if newmem.count('blacklist') > 0 and mem['from'] not in mem_memo  : 
+                b_list = blist.append(mem['timestamp'] + '|' + mem['from'] + '|' +  mem['memo'] +' <br>') 
+                mem_memo.add(mem['from'])
+    return blist
+
